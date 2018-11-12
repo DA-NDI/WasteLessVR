e716bcb2e9372aa95351829b6ca79ef3a63d0942
Andrii <andriyvolgin@gmail.com>
2018-11-12 15:26:23 +0200

    update to scene1. score and containers

---
+++ Assets/Scripts/FollowSteamCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSteamCamera : MonoBehaviour {
+    public GameObject camera_steam;
+    public GameObject camera_simulator;
+    private Vector3 offset;
+    
+	void Start ()
+    {
+        if (camera_steam)
+            offset = transform.position - camera_steam.transform.position;
+        else if (camera_simulator)
+            offset = transform.position - camera_simulator.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (camera_steam)
+            transform.position = camera_steam.transform.position + offset;
+        else if (camera_simulator)
+            transform.position = camera_simulator.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/NonRecycleScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonRecycleScore : MonoBehaviour {
+	public ScoreBoard scoreBoard;
+	void OnTriggerEnter(Collider col){	
+		switch(col.tag){
+			case "MetalTrash":
+                {
+                    Debug.Log("Catched metal");
+                    scoreBoard.handleWrongScored();
+                    break;
+                }
+			case "PlasticTrash":
+				scoreBoard.handleWrongScored();
+				break;
+			case "PaperTrash":
+				scoreBoard.handleWrongScored();
+				break;
+			case "GlassTrash":
+				scoreBoard.handleWrongScored();
+				break;
+			case "Waste":
+				scoreBoard.handleScored();
+				break;
+		}	
+			Trash trash = col.gameObject.GetComponent<Trash>();
+			trash.setSpeedToZero();
+	}
+}
