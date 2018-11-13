98ecc2314048aafa260817a8670a88e07f830bd7
Svitlana S <svitlana.shymko@gmail.com>
2018-11-13 02:41:18 +0200

    added menu mockup

--- Assets/Scripts/DestroyTrash.cs
+++ Assets/Scripts/DestroyTrash.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using VRTK;
 
 public class DestroyTrash : MonoBehaviour {
 	public float destroyTime = 20.0f;
 
 	void Update () {
-		// if(!this.gameObject.isGrabbed()){
+		// if(!this.gameObject.isGrabbed()){???
 			Destroy(this.gameObject, destroyTime);
 	}
 }
