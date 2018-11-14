954ee5c714439a4fabed3cf27b5633dec83528a6
Andrii <andriyvolgin@gmail.com>
2018-11-14 18:25:48 +0200

    scene1 pointer working

---
+++ Assets/Scripts/SetActiveScript42.cs
@@ -0,0 +1,62 @@
+﻿namespace VRTK.Examples
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SetActiveScript42 : MonoBehaviour
+    {
+        public GameObject cube;
+        protected bool activate;
+        public VRTK_InteractableObject linkedObject;
+
+
+        protected virtual void OnEnable()
+        {
+            activate = false;
+            linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+            if (linkedObject != null)
+            {
+                linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+                linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+            }
+
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (linkedObject != null)
+            {
+                Debug.Log("i'm enabled");
+                linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+                linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
+            }
+        }
+
+        protected virtual void Update()
+        {
+            if (activate)
+            {
+                cube.SetActive(true);
+            } else if (!activate)
+            {
+                cube.SetActive(false);
+            }
+        }
+
+        protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+        {
+            activate = true;
+        }
+
+        protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+        {
+            activate = false;
+        }
+    }
+}
+
+
+
+
